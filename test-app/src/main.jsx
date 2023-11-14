8c038ab7a41f11a3b3db58d70989b41ad9779eed
AlexPetrov88 <112386979+AlexPetrov88@users.noreply.github.com>
2023-11-14 15:54:17 +0200

    Migrate to Vite

--- test-app/src/main.jsx
+++ test-app/src/main.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
-import App from './App';
-
+import App from './App.jsx';
+import store from './store/store.js';
 import './index.css';
-import store from './store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
