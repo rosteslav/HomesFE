d584a8e2bb45acb44a95988aa5af14b388c4106b
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-11-14 15:10:27 +0000

    remove router

--- test-app/src/main.jsx
+++ test-app/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './store/store.js';
@@ -8,10 +7,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
