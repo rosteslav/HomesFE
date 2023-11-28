6ac091c5670febe47cd37c7b175dbadb4629c0e1
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-11-28 16:39:31 +0000

    change imageScroller path and send property images

--- test-app/src/components/CatalogItems/CatalogItem/CatalogItem.jsx
+++ test-app/src/components/CatalogItems/CatalogItem/CatalogItem.jsx
@@ -1,12 +1,12 @@
 import CardFooter from './CardFooter';
 import CardHeader from './CardHeader';
-import ImageScroll from './ImageScroll';
+import ImageScroll from '../../../UI/ImageScroll';
 
-export const CatalogItem = ({ property, images }) => {
+export const CatalogItem = ({ property }) => {
     return (
         <div>
             <CardHeader property={property} />
-            <ImageScroll images={images} propertyId={property.id} />
+            <ImageScroll images={property.images} propertyId={property.id} />
             <CardFooter property={property} />
         </div>
     );
