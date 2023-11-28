228acac49f730b06f23a14c285c0f3f54f32b5e8
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-11-28 16:33:07 +0000

    refactor

--- test-app/src/UI/auth.js
+++ test-app/src/UI/auth.js
@@ -1,7 +1,12 @@
 import { redirect } from 'react-router-dom';
 
+export const getUserData = () => {
+    return JSON.parse(localStorage.getItem('authToken'));
+};
+
 export const restrictLoginRegister = async () => {
-    const user = JSON.parse(localStorage.getItem('authToken'));
+    const user = getUserData();
+    console.log(user);
     if (user) {
         return redirect('/');
     }
