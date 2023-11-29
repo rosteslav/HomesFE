55d51d134858560b16bc0907739dfa1d20c88c3f
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-11-29 08:24:07 +0000

    remove mock data and use images from property

--- test-app/src/components/CatalogItems/PropertiesDetails/DetailsImages.jsx
+++ test-app/src/components/CatalogItems/PropertiesDetails/DetailsImages.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const DetailsImages = ({ images }) => {
-    const [mainImage, setMainImage] = useState(images[0]);
+    const [mainImage, setMainImage] = useState();
+
+    useEffect(() => {
+        if (images && images.length > 0) {
+            setMainImage(images[0]);
+        }
+    }, [images]);
 
     return (
         <div>
-            <img src={mainImage} className='h-60 w-full  object-cover lg:h-96' />
+            {images && images.length > 0 && (
+                <img src={mainImage} className='h-60 w-full  object-cover lg:h-96' />
+            )}
             <div className='mt-4 grid grid-cols-4 gap-4'>
-                {images.map((image, inx) => (
-                    <img
-                        className='h-24 w-full cursor-pointer object-cover lg:h-36'
-                        src={image}
-                        key={inx}
-                        onClick={() => setMainImage(image)}
-                    />
-                ))}
+                {images &&
+                    images.length > 0 &&
+                    images.map((image, inx) => (
+                        <img
+                            className='h-24 w-full cursor-pointer object-cover lg:h-36'
+                            src={image}
+                            key={inx}
+                            onClick={() => setMainImage(image)}
+                        />
+                    ))}
             </div>
         </div>
     );
