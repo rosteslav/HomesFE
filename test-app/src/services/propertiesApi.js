714bba786b9d10f616f9f89e366f25aa28c186d6
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-03 19:15:51 +0000

    create baseQuery and fetchAllProperties query

--- test-app/src/services/propertiesApi.js
+++ test-app/src/services/propertiesApi.js
@@ -1,12 +1,25 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const propertiesApi = createApi({
     reducerPath: 'propertiesApi',
-    
-    endpoints() {
-        return {};
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5221',
+        prepareHeaders: async (headers, { getState }) => {
+            const token = await getState().auth.data.token?.token;
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`);
+            }
+            return headers;
+        },
+    }),
+    endpoints(builder) {
+        return {
+            fetchAllProperties: builder.query({
+                query: () => ({ url: '/properties/all' }),
+            }),
+        };
     },
 });
 
+export const { useFetchAllPropertiesQuery } = propertiesApi;
 export { propertiesApi };
-
