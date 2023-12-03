7c43e22bd650c55a9d64297a19b237e54bdfc7aa
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-03 20:11:20 +0000

    add imageApi slice

--- test-app/src/store/store.js
+++ test-app/src/store/store.js
@@ -5,6 +5,7 @@ import registerUserSlice from './slices/registerUserSlice/registerUserSlice';
 import registerAdminSlice from './slices/registerAdminSlice/registerAdminSlice';
 import { propertiesApi } from '../services/propertiesApi';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import { imagesApi } from '../services/imagesApi';
 
 const store = configureStore({
     reducer: {
@@ -13,9 +14,12 @@ const store = configureStore({
         registerUserForm: registerUserSlice,
         registerAdmin: registerAdminSlice,
         [propertiesApi.reducerPath]: propertiesApi.reducer,
+        [imagesApi.reducerPath]: imagesApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware({ serializableCheck: false }).concat(propertiesApi.middleware);
+        return getDefaultMiddleware({ serializableCheck: false })
+            .concat(propertiesApi.middleware)
+            .concat(imagesApi.middleware);
     },
 });
 setupListeners(store.dispatch);
