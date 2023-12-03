a0eae81d963323cd229a31eb701ef4db4a544785
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-03 20:10:15 +0000

    create imageSlice with create image mutation

---
+++ test-app/src/services/imagesApi.js
@@ -0,0 +1,54 @@
+import { propertiesApi } from './propertiesApi';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const imagesApi = createApi({
+    reducerPath: 'imagesApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5221',
+        prepareHeaders: async (headers, { getState }) => {
+            const token = await getState().auth.data.token?.token;
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`);
+            }
+            return headers;
+        },
+    }),
+    endpoints(builder) {
+        return {
+            addPropertyImage: builder.mutation({
+                query: (arg) => {
+                    return {
+                        url: `/image?propertyId=${arg.propertyId}`,
+                        method: 'POST',
+                        body: arg.data,
+                    };
+                },
+                async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                    try {
+                        const res = await queryFulfilled;
+
+                        dispatch(
+                            propertiesApi.util.updateQueryData(
+                                'fetchOwnProperties',
+                                undefined,
+                                (draftData) => {
+                                    draftData?.map((property) => {
+                                        if (property.id === arg.propertyId) {
+                                            property.images.push(res.data.imageURL);
+                                        }
+                                        return property;
+                                    });
+                                }
+                            )
+                        );
+                    } catch (error) {
+                        console.log(error);
+                    }
+                },
+            }),
+        };
+    },
+});
+
+export const { useAddPropertyImageMutation } = imagesApi;
+export { imagesApi };
