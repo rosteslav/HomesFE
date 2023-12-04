ba2d56e5b135450026af9433a90d89805102ad6f
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-04 09:22:47 +0000

    create delete property, fetch property by id, and all options

--- test-app/src/services/propertiesApi.js
+++ test-app/src/services/propertiesApi.js
@@ -20,6 +20,15 @@ const propertiesApi = createApi({
             fetchOwnProperties: builder.query({
                 query: () => ({ url: '/properties' }),
             }),
+            fetchPropertyOptions: builder.query({
+                query: () => ({ url: '/propertyOptions' }),
+            }),
+            fetchPropertiesFilterOptions: builder.query({
+                query: () => ({ url: '/propertyOptions/filter' }),
+            }),
+            fetchPropertyById: builder.query({
+                query: (id) => ({ url: `/properties/${id}` }),
+            }),
             addPropertyInfo: builder.mutation({
                 query: (formData) => {
                     return {
@@ -42,6 +51,50 @@ const propertiesApi = createApi({
                                 }
                             )
                         );
+                        dispatch(
+                            propertiesApi.util.updateQueryData(
+                                'fetchAllProperties',
+                                undefined,
+                                (draftData) => {
+                                    draftData?.push(data);
+                                }
+                            )
+                        );
+                    } catch (error) {
+                        console.log(error);
+                    }
+                },
+            }),
+            deleteOwnProperty: builder.mutation({
+                query: (id) => {
+                    return {
+                        url: `/properties/${id}`,
+                        method: 'DELETE',
+                    };
+                },
+                async onQueryStarted(data, { dispatch, queryFulfilled }) {
+                    try {
+                        await queryFulfilled;
+                        console.log(data);
+
+                        dispatch(
+                            propertiesApi.util.updateQueryData(
+                                'fetchOwnProperties',
+                                undefined,
+                                (draftData) => {
+                                    return draftData?.filter((property) => property?.id !== data);
+                                }
+                            )
+                        );
+                        dispatch(
+                            propertiesApi.util.updateQueryData(
+                                'fetchAllProperties',
+                                undefined,
+                                (draftData) => {
+                                    return draftData?.filter((property) => property?.id !== data);
+                                }
+                            )
+                        );
                     } catch (error) {
                         console.log(error);
                     }
@@ -54,6 +107,10 @@ const propertiesApi = createApi({
 export const {
     useFetchAllPropertiesQuery,
     useFetchOwnPropertiesQuery,
+    useFetchPropertyOptionsQuery,
+    useFetchPropertiesFilterOptionsQuery,
+    useFetchPropertyByIdQuery,
     useAddPropertyInfoMutation,
+    useDeleteOwnPropertyMutation,
 } = propertiesApi;
 export { propertiesApi };
