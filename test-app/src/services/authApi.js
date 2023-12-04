30d2baa00956e41a182f45baa6261cf867a8b6d5
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-04 18:26:48 +0000

    create brokersList mutation

--- test-app/src/services/authApi.js
+++ test-app/src/services/authApi.js
@@ -1,11 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setUser } from '../store/features/authUser';
-import { propertiesApi } from './propertiesApi';
 
 const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:5220',
+        prepareHeaders: async (headers, { getState }) => {
+            const token = await getState().auth.data.token?.token;
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`);
+            }
+            return headers;
+        },
     }),
     endpoints(builder) {
         return {
@@ -20,7 +26,6 @@ const authApi = createApi({
                 async onQueryStarted(data, { dispatch, queryFulfilled }) {
                     try {
                         const response = await queryFulfilled;
-                        console.log(response);
                         dispatch(setUser(response.data));
                     } catch (error) {
                         console.log(error);
@@ -35,27 +40,21 @@ const authApi = createApi({
                         body,
                     };
                 },
-                // async onQueryStarted(data, { dispatch, queryFulfilled }) {
-                //     console.log(data);
-                //     try {
-                //         await queryFulfilled;
-                //         dispatch(
-                //             authApi.util.('login', undefined, {
-                //                 username: data.username,
-                //                 password: data.password,
-                //             })
-                //         );
-                //     } catch (error) {
-                //         console.log(error);
-                //     }
-                // },
             }),
             fetchRolesOptions: builder.query({
                 query: () => ({ url: '/roles' }),
+            }),
+            fetchBrokersOptions: builder.query({
+                query: () => ({ url: 'authOptions/brokers' }),
             }),
         };
     },
 });
 
-export const { useLoginMutation, useFetchRolesOptionsQuery, useRegisterUserMutation } = authApi;
+export const {
+    useLoginMutation,
+    useFetchRolesOptionsQuery,
+    useRegisterUserMutation,
+    useFetchBrokersOptionsQuery,
+} = authApi;
 export { authApi };
