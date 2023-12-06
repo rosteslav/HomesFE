c21afb9972322d007d6b73f9e511030034b27f75
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-06 08:41:31 +0000

    use filter state

--- test-app/src/components/CatalogItems/CatalogFilter.jsx
+++ test-app/src/components/CatalogItems/CatalogFilter.jsx
@@ -1,35 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { useFetchPropertiesFilterOptionsQuery } from '../../services/propertiesApi';
-import { MainButton, OptionButton } from '../../UI/ButtonsFilter';
-import { useState } from 'react';
+import ButtonFilter from '../../UI/ButtonsFilter';
+import { useEffect, useState } from 'react';
+import { loadAllOptions, setFilterOption } from '../../store/features/filter';
 
-const CatalogFilter = ({ refetch, setFetchData }) => {
-    const { data: propertiesFilterOptions } = useFetchPropertiesFilterOptionsQuery();
+const CatalogFilter = ({ setFetchData }) => {
+    const dispatch = useDispatch();
+    const { data: propertiesFilterOptions, isSuccess } = useFetchPropertiesFilterOptionsQuery();
+    const filter = useSelector((state) => state.filter);
+
     const [option, setOption] = useState();
-    const [buttonValues, setButtonValues] = useState({
-        neighbourhood: ['Квартали'],
-        numberOfRooms: ['Тип / Брой стаи'],
-        buildingType: ['Вид строителството'],
-        exposure: ['Изложение'],
-        finish: ['Завършеност на имота'],
-        furnishment: ['Обзавеждане'],
-        heating: ['Отопление'],
+
+    useEffect(() => {
+        if (isSuccess) {
+            dispatch(loadAllOptions(propertiesFilterOptions));
+        }
     });
 
     const optionsHandler = (option, value) => {
-        setButtonValues((prevButtonValues) => {
-            const data = prevButtonValues[option].slice();
-            const index = data.indexOf(value);
-
-            if (index !== -1) {
-                data.splice(index, 1);
-            } else {
-                data.push(value);
-            }
-            return {
-                ...prevButtonValues,
-                [option]: [...data],
-            };
-        });
+        dispatch(setFilterOption({ option, value }));
     };
 
     const buttonsContainer = (option) => {
@@ -37,13 +26,17 @@ const CatalogFilter = ({ refetch, setFetchData }) => {
             return <></>;
         }
 
-        if (option && propertiesFilterOptions[option]) {
+        if (option && filter.filter.data[option].allOptions) {
             return (
                 <>
-                    {propertiesFilterOptions[option].map((x) => (
-                        <OptionButton action={() => optionsHandler(option, x)} key={x}>
+                    {filter.filter.data[option].allOptions.map((x) => (
+                        <ButtonFilter
+                            isActive={filter.queryData[option].includes(x) ? true : false}
+                            action={() => optionsHandler(option, x)}
+                            key={x}
+                        >
                             {x}
-                        </OptionButton>
+                        </ButtonFilter>
                     ))}
                 </>
             );
@@ -54,38 +47,60 @@ const CatalogFilter = ({ refetch, setFetchData }) => {
     const refetchHandler = (e) => {
         const targetTagName = e.target.tagName;
         if (targetTagName !== 'BUTTON') {
-            // console.log(option);
             setOption(undefined);
-            setFetchData(buttonValues)
+            setFetchData(filter.queryData);
         }
     };
 
     return (
         <>
             <div className='my-4 border-b-2 border-t-2 border-black'>
-                <div className='flex flex-wrap'>
-                    <MainButton action={() => setOption('neighbourhood')}>
-                        {buttonValues.neighbourhood}
-                    </MainButton>
-                    <MainButton action={() => setOption('numberOfRooms')}>
-                        {buttonValues.numberOfRooms}
-                    </MainButton>
-                    <MainButton action={() => setOption('buildingType')}>
-                        {buttonValues.buildingType}
-                    </MainButton>
-                    <MainButton action={() => setOption('exposure')}>
-                        {buttonValues.exposure}
-                    </MainButton>
-                    <MainButton action={() => setOption('finish')}>
-                        {buttonValues.finish}
-                    </MainButton>
-                    <MainButton action={() => setOption('furnishment')}>
-                        {buttonValues.furnishment}
-                    </MainButton>
-                    <MainButton action={() => setOption('heating')}>
-                        {buttonValues.heating}
-                    </MainButton>
-                </div>
+                {isSuccess && (
+                    <div className='flex flex-wrap'>
+                        <ButtonFilter
+                            isActive={filter.queryData.neighbourhood.length > 0 ? true : false}
+                            action={() => setOption('neighbourhood')}
+                        >
+                            {filter.filter.data.neighbourhood.buttonContent}
+                        </ButtonFilter>
+                        <ButtonFilter
+                            isActive={filter.queryData.numberOfRooms.length > 0 ? true : false}
+                            action={() => setOption('numberOfRooms')}
+                        >
+                            {filter.filter.data.numberOfRooms.buttonContent}
+                        </ButtonFilter>
+                        <ButtonFilter
+                            isActive={filter.queryData.buildingType.length > 0 ? true : false}
+                            action={() => setOption('buildingType')}
+                        >
+                            {filter.filter.data.buildingType.buttonContent}
+                        </ButtonFilter>
+                        <ButtonFilter
+                            isActive={filter.queryData.exposure.length > 0 ? true : false}
+                            action={() => setOption('exposure')}
+                        >
+                            {filter.filter.data.exposure.buttonContent}
+                        </ButtonFilter>
+                        <ButtonFilter
+                            isActive={filter.queryData.finish.length > 0 ? true : false}
+                            action={() => setOption('finish')}
+                        >
+                            {filter.filter.data.finish.buttonContent}
+                        </ButtonFilter>
+                        <ButtonFilter
+                            isActive={filter.queryData.furnishment.length > 0 ? true : false}
+                            action={() => setOption('furnishment')}
+                        >
+                            {filter.filter.data.furnishment.buttonContent}
+                        </ButtonFilter>
+                        <ButtonFilter
+                            isActive={filter.queryData.heating.length > 0 ? true : false}
+                            action={() => setOption('heating')}
+                        >
+                            {filter.filter.data.heating.buttonContent}
+                        </ButtonFilter>
+                    </div>
+                )}
             </div>
             <div onClick={refetchHandler}>{buttonsContainer(option)}</div>
         </>
