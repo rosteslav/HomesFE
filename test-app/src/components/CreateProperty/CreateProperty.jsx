5cb36f23f4782aa7e614403e57655ad36aec8e85
AlexPetrov88 <112386979+AlexPetrov88@users.noreply.github.com>
2023-12-07 18:13:01 +0200

    Update CreateProperty.jsx

--- test-app/src/components/CreateProperty/CreateProperty.jsx
+++ test-app/src/components/CreateProperty/CreateProperty.jsx
@@ -504,7 +504,7 @@ export const CreateProperty = () => {
                                                 value={option}
                                                 onChange={handleExposureChange}
                                                 checked={selectedExposure.includes(option)}
-                                                className='exposure1 peer mb-2 me-2 mt-1 rounded-full border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700'
+                                                className='exposure1 hidden peer mb-2 me-2 mt-1 rounded-full border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700'
                                             />
                                             <label
                                                 htmlFor={index}
