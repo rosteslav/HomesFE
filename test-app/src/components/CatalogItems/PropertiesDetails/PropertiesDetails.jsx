671d77dff0164d5184c9ebef713629f07b9cc171
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-09 11:09:56 +0000

    check is any error and if there is not property redirect to home page and throw toast error, add skeleton when isLoading is true, and add more info

--- test-app/src/components/CatalogItems/PropertiesDetails/PropertiesDetails.jsx
+++ test-app/src/components/CatalogItems/PropertiesDetails/PropertiesDetails.jsx
@@ -1,24 +1,36 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 
-import Loader from '../../../UI/Loader';
 import DetailsImages from './DetailsImages';
 import { useFetchPropertyByIdQuery } from '../../../services/propertiesApi';
+import notificationMessages from '../../../services/notificationMessages';
+import { TextSkeleton } from '../../../UI/Skeletons';
 
 export const PropertiesDetails = () => {
     const { detailsId } = useParams();
-    const { data: property, isLoading } = useFetchPropertyByIdQuery(detailsId);
+    const { data: property, isLoading, isError, error } = useFetchPropertyByIdQuery(detailsId);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isError) {
+            navigate('/');
+            toast.error(notificationMessages(error.status));
+        }
+    });
 
     const pricePerSqm = property?.price / property?.space;
 
     return (
         <section className='m-4 mt-10'>
-            {isLoading && <Loader />}
             <h1 className='text-3xl font-semibold'>
                 {property?.numberOfRooms} апартамент за продажба, {property?.space} m<sup>2</sup>
             </h1>
             <div className='mt-10 grid grid-cols-1 gap-6 md:grid-cols-2'>
                 <DetailsImages images={property?.images} isLoading={isLoading} />
+                {isLoading}
                 <div className='text-xl'>
+                    {isLoading && <TextSkeleton times={20} />}
                     <div className='flex justify-between'>
                         <p className='font-semibold'>София</p>
                         <p>
@@ -51,13 +63,23 @@ export const PropertiesDetails = () => {
                         {property?.heating}
                     </p>
                     <p>
+                        <span className='font-bold'>Завършеност: </span>
+                        {property?.finish}
+                    </p>
+                    <p>
+                        <span className='font-bold'>Гараж: </span>
+                        {property?.garage}
+                    </p>
+                    <p>
                         <span className='font-bold'>Строителство: </span>
                         {property?.buildingType}
                     </p>
-                    {property && property.exposure && <p>
-                        <span className='font-bold'>Изложение: </span>
-                        {property?.exposure}
-                    </p> }
+                    {property && property.exposure && (
+                        <p>
+                            <span className='font-bold'>Изложение: </span>
+                            {property?.exposure}
+                        </p>
+                    )}
                     <p className='border-b-2 border-black pt-4 font-bold'>
                         Допълнителна информация
                     </p>
