3cbeb0af8ac80a5b97f0d534118fa30e70fab0d0
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-10 12:14:55 +0000

    add arrow svgs

--- test-app/src/components/CatalogItems/CatalogFilter.jsx
+++ test-app/src/components/CatalogItems/CatalogFilter.jsx
@@ -2,9 +2,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
 import { useFetchPropertiesFilterOptionsQuery } from '../../services/propertiesApi';
-import ButtonFilter from '../../UI/ButtonsFilter';
+import { ButtonFilter, ButtonReset } from '../../UI/ButtonsFilter';
 import {
     loadAllOptions,
+    resetFilter,
     setFilterOption,
     updateFilterQueryData,
 } from '../../store/features/filter';
@@ -25,6 +26,7 @@ const CatalogFilter = () => {
 
     const optionsHandler = (option, value) => {
         dispatch(setFilterOption({ option, value }));
+        dispatch(updateFilterQueryData());
     };
 
     const selectedOptions = (option) => {
@@ -53,7 +55,7 @@ const CatalogFilter = () => {
                 </div>
             );
         } else if (option?.singleChoice) {
-            const key = option.singleChoice == 'orderBy' ? 'relatedPropName' : 'numberOfDays'
+            const key = option.singleChoice == 'orderBy' ? 'relatedPropName' : 'numberOfDays';
             return filter.filter.data[option.singleChoice].allOptions.map((button) => (
                 <ButtonFilter
                     isActive={
@@ -64,11 +66,42 @@ const CatalogFilter = () => {
                     action={() => {
                         optionsHandler(option.singleChoice, button);
                         setOption(undefined);
-                        dispatch(updateFilterQueryData())
+                        dispatch(updateFilterQueryData());
                     }}
                     key={button[key]}
                 >
                     {button.description}
+                    {button.isAscending ? (
+                        <svg
+                            className='ms-2.5 h-4 w-4'
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 24 24'
+                            fill='none'
+                        >
+                            <path
+                                d='M12 6V18M12 18L7 13M12 18L17 13'
+                                stroke='#000000'
+                                strokeWidth='4'
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                            />
+                        </svg>
+                    ) : (
+                        <svg
+                            className='ms-2.5 h-4 w-4'
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 24 24'
+                            fill='none'
+                        >
+                            <path
+                                d='M12 6V18M12 6L7 11M12 6L17 11'
+                                stroke='#000000'
+                                strokeWidth='4'
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                            />
+                        </svg>
+                    )}
                 </ButtonFilter>
             ));
         }
@@ -78,9 +111,13 @@ const CatalogFilter = () => {
 
     const refetchHandler = (e) => {
         const targetTagName = e.target.tagName;
+        const className = e.target.className;
+        console.log(className.includes('slider'));
         if (targetTagName !== 'BUTTON') {
+            if (className.includes('slider')) {
+                return;
+            }
             setOption(undefined);
-            dispatch(updateFilterQueryData());
         }
     };
 
@@ -88,7 +125,7 @@ const CatalogFilter = () => {
         <>
             <div className='my-4 border-b-2 border-t-2 border-black'>
                 {isSuccess && (
-                    <div className='mx-8 flex flex-wrap'>
+                    <div className='mx-8 flex flex-wrap items-center justify-between'>
                         <ButtonFilter
                             isActive={
                                 option?.multiChoice == 'neighbourhood' ||
@@ -219,19 +256,26 @@ const CatalogFilter = () => {
                         >
                             {filter.filter.data.publishedOn.buttonContent}
                         </ButtonFilter>
-                        <ButtonFilter
-                            isActive={
-                                option?.singleChoice == 'orderBy' ||
-                                filter.filter.data.orderBy.options.length > 0
-                                    ? true
-                                    : false
-                            }
-                            action={() => {
-                                setOption({ singleChoice: 'orderBy' });
-                            }}
-                        >
-                            {filter.filter.data.orderBy.buttonContent}
-                        </ButtonFilter>
+
+                        <div className='flex flex-1 items-center justify-end'>
+                            <ButtonFilter
+                                isActive={
+                                    option?.singleChoice == 'orderBy' ||
+                                    filter.filter.data.orderBy.options.length > 0
+                                        ? true
+                                        : false
+                                }
+                                action={() => {
+                                    setOption({ singleChoice: 'orderBy' });
+                                }}
+                            >
+                                {filter.filter.data.orderBy.buttonContent}
+                            </ButtonFilter>
+
+                            <ButtonReset action={() => dispatch(resetFilter())}>
+                                Премахни филтрите
+                            </ButtonReset>
+                        </div>
                     </div>
                 )}
             </div>
