1679c399887fd81e414e6ddab5c63a7cda392ef4
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-12 07:27:48 +0000

    add onClick handler

--- test-app/src/UI/ButtonPrimary.jsx
+++ test-app/src/UI/ButtonPrimary.jsx
@@ -1,6 +1,12 @@
-const ButtonPrimary = ({ children }) => {
+const ButtonPrimary = ({ children, action }) => {
+
+    const handleClick = () => {
+        if (action) {
+            action();
+        }
+    };
     return (
-        <button className='text-md flex  w-full justify-center rounded-md border border-indigo-600 bg-indigo-600 px-3 py-1.5 font-bold leading-6 tracking-wider text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+        <button onClick={handleClick} className='text-md flex  w-full justify-center rounded-md border border-indigo-600 bg-indigo-600 px-3 py-1.5 font-bold leading-6 tracking-wider text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
             {children}
         </button>
     );
