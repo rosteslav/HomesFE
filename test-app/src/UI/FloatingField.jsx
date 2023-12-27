3af9b5acff87cbfe8c15c874c2ed871ee0c7931e
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-27 15:15:58 +0000

    change text color for better contrast

--- test-app/src/UI/FloatingField.jsx
+++ test-app/src/UI/FloatingField.jsx
@@ -33,7 +33,7 @@ const FloatingField = ({
                     className={`absolute start-3 top-3 z-10 origin-[0] -translate-y-5 scale-75 transform ${
                         errors[name] &&
                         'text-red-600 peer-focus:text-red-600 dark:text-red-400 peer-focus:dark:text-red-500'
-                    } bg-stone-100 px-2 text-lg text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-3 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:bg-gray-900 dark:text-gray-400 peer-focus:dark:text-blue-500`}
+                    } bg-stone-100 px-2 text-lg text-gray-900 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-3 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:bg-gray-900 dark:text-gray-400 peer-focus:dark:text-blue-500`}
                 >
                     {placeholder}
                 </label>
