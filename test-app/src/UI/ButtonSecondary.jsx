8e5310134989273319c041df2803082315dc0907
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-28 05:49:28 +0000

    add type for button

--- test-app/src/UI/ButtonSecondary.jsx
+++ test-app/src/UI/ButtonSecondary.jsx
@@ -1,4 +1,4 @@
-const ButtonSecondary = ({ children, action }) => {
+const ButtonSecondary = ({ children, action, isSubmit }) => {
     const handleClick = () => {
         if (action) {
             action();
@@ -7,7 +7,7 @@ const ButtonSecondary = ({ children, action }) => {
     return (
         <button
             onClick={handleClick}
-            type="button"
+            type={isSubmit ? 'submit' : 'button'}
             className='text-md flex w-full justify-center rounded-md border border-indigo-600 px-3 py-1.5 font-semibold leading-6 tracking-wider text-indigo-600 shadow-sm hover:bg-indigo-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
         >
             {children}
