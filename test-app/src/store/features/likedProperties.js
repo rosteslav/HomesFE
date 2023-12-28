5d07b675ef88911898c4d7f66110e3d20b612e6d
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-28 14:13:44 +0000

    create filter reducer slice

---
+++ test-app/src/store/features/likedProperties.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+import Cookies from 'universal-cookie';
+
+const cookie = new Cookies();
+
+const likedPropertiesSlice = createSlice({
+    name: 'likedProperties',
+    initialState: { data: [] },
+    reducers: {
+        loadLikedProperties(state) {
+            const existingCookie = cookie.get('allLikedProperties');
+            if (existingCookie) {
+                state.data = existingCookie;
+            } else {
+                state.data = [];
+                cookie.set('allLikedProperties', []);
+            }
+        },
+        changeLikedProperties(state, action) {
+            const likedProperties = state.data;
+            const indexOf = likedProperties.indexOf(action.payload);
+            if (indexOf == -1) {
+                likedProperties.push(action.payload);
+            } else {
+                likedProperties.splice(indexOf, 1);
+            }
+            state.data = likedProperties;
+            cookie.set('allLikedProperties', state.data);
+        },
+    },
+});
+
+export default likedPropertiesSlice.reducer;
+export const { changeLikedProperties, loadLikedProperties } = likedPropertiesSlice.actions;
