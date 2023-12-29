e96fe3f5736dfe67741922e9632836b27a66da03
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-29 20:26:56 +0000

    add new components

--- test-app/src/components/AdminDashboard/Dashboard.jsx
+++ test-app/src/components/AdminDashboard/Dashboard.jsx
@@ -1,30 +1,42 @@
 import { useState } from 'react';
 import { ButtonPrimary } from '../../UI';
+import AllPropertiesDashboard from './AllPropertiesDashboard';
+import ReportedPropertiesDashboard from './ReportedPropertiesDashboard';
+import ChangeNeighborhoodsDashboard from './ChangeNeighborhoodsDashboard';
 
 const Dashboard = () => {
-    const [action, setAction] = useState()
+    const [action, setAction] = useState();
 
     const handleButtonAction = (e) => {
-        setAction(e)
-    }
+        setAction(e);
+    };
 
     return (
         <div className=''>
             <nav className='fixed h-full bg-gray-200 px-3'>
                 <ul>
                     <li className='mb-4'>
-                        <ButtonPrimary action={() => handleButtonAction('Всички имоти')}>Всички имоти</ButtonPrimary>
+                        <ButtonPrimary action={() => handleButtonAction('allProperties')}>
+                            Всички имоти
+                        </ButtonPrimary>
                     </li>
                     <li className='mb-4'>
-                        <ButtonPrimary action={() => handleButtonAction('Проблемни имоти')}>Проблемни имоти</ButtonPrimary>
+                        <ButtonPrimary action={() => handleButtonAction('reportedProperties')}>
+                            Проблемни имоти
+                        </ButtonPrimary>
                     </li>
                     <li className='mb-4'>
-                        <ButtonPrimary action={() => handleButtonAction('Редактиране на квартали')}>Редактиране на квартали</ButtonPrimary>
+                        <ButtonPrimary action={() => handleButtonAction('changeNeighbourhood')}>
+                            Редактиране на квартали
+                        </ButtonPrimary>
                     </li>
                 </ul>
             </nav>
-            <div className='m-auto w-full'>
-                <p className='text-center'>{action}</p>
+            <div className='pl-72'>
+                {!action && <p className='text-center'>Добре дошли в админ панела.</p>}
+                {action === 'allProperties' && <AllPropertiesDashboard />}
+                {action === 'reportedProperties' && <ReportedPropertiesDashboard />}
+                {action === 'changeNeighbourhood' && <ChangeNeighborhoodsDashboard />}
             </div>
         </div>
     );
