5fbbc36b3464927aad91226044426436cc2b7f9e
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-29 20:09:46 +0000

    add action buttons and state for action content

--- test-app/src/components/AdminDashboard/Dashboard.jsx
+++ test-app/src/components/AdminDashboard/Dashboard.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+import { ButtonPrimary } from '../../UI';
+
 const Dashboard = () => {
-    return <h1>Dashboard</h1>;
+    const [action, setAction] = useState()
+
+    const handleButtonAction = (e) => {
+        setAction(e)
+    }
+
+    return (
+        <div className=''>
+            <nav className='fixed h-full bg-gray-200 px-3'>
+                <ul>
+                    <li className='mb-4'>
+                        <ButtonPrimary action={() => handleButtonAction('Всички имоти')}>Всички имоти</ButtonPrimary>
+                    </li>
+                    <li className='mb-4'>
+                        <ButtonPrimary action={() => handleButtonAction('Проблемни имоти')}>Проблемни имоти</ButtonPrimary>
+                    </li>
+                    <li className='mb-4'>
+                        <ButtonPrimary action={() => handleButtonAction('Редактиране на квартали')}>Редактиране на квартали</ButtonPrimary>
+                    </li>
+                </ul>
+            </nav>
+            <div className='m-auto w-full'>
+                <p className='text-center'>{action}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
