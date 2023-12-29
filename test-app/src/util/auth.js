2ec143318f1de455c6e4ad9d60da18575670c705
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2023-12-29 18:20:28 +0000

    create function to read token and to return the data in object

--- test-app/src/util/auth.js
+++ test-app/src/util/auth.js
@@ -1,4 +1,5 @@
 import { redirect } from 'react-router-dom';
+import { decodeToken } from 'react-jwt';
 
 export const getUserData = () => {
     return JSON.parse(localStorage.getItem('authToken'));
@@ -12,4 +13,17 @@ export const restrictLoginRegister = async () => {
     }
 
     return null;
+};
+
+export const readToken = (token) => {
+    const userData = {};
+    const decodedToken = decodeToken(token);
+
+    for (const key in decodedToken) {
+        const currKey = key.split('/');
+        if (!Object.prototype.hasOwnProperty.call(userData, currKey[currKey.length - 1])) {
+            userData[currKey[currKey.length - 1]] = decodedToken[key];
+        }
+    }
+    return userData;
 };
