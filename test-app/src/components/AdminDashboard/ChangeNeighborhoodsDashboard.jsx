b2d0ebcbf91b32a1437c542b0feee300bb903ebe
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-07 08:58:50 +0000

    fetch all ratings and create update

--- test-app/src/components/AdminDashboard/ChangeNeighborhoodsDashboard.jsx
+++ test-app/src/components/AdminDashboard/ChangeNeighborhoodsDashboard.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SofiaSvgMap from './SofiaSvgMap';
 import { ButtonPrimary } from '../../UI';
+import {
+    useFetchNeighbourhoodsRatingQuery,
+    useUpdateNeighbourhoodsRatingMutation,
+} from '../../services/adminApi';
 
 const ChangeNeighborhoodsDashboard = () => {
+    const { data: neighbourhoodsRating } = useFetchNeighbourhoodsRatingQuery();
+    const [updateNeighbourhoodsRating] = useUpdateNeighbourhoodsRatingMutation([[], []]);
     const [forLiving, setForLiving] = useState([[], []]);
     const [forInvestment, setForInvestment] = useState([[], []]);
     const [forBudget, setForBudget] = useState([[], []]);
     const [forLuxurious, setForLuxurious] = useState([[], []]);
+
+    useEffect(() => {
+        if (neighbourhoodsRating) {
+            setForLiving([
+                [...neighbourhoodsRating.for_living[0]],
+                [...neighbourhoodsRating.for_living[1]],
+            ]);
+            setForInvestment([
+                [...neighbourhoodsRating.for_investment[0]],
+                [...neighbourhoodsRating.for_investment[1]],
+            ]);
+            setForBudget([
+                [...neighbourhoodsRating.budget[0]],
+                [...neighbourhoodsRating.budget[1]],
+            ]);
+            setForLuxurious([
+                [...neighbourhoodsRating.luxury[0]],
+                [...neighbourhoodsRating.luxury[1]],
+            ]);
+        }
+    }, [neighbourhoodsRating]);
 
     const handleResult = () => {
         const result = {
@@ -15,32 +42,32 @@ const ChangeNeighborhoodsDashboard = () => {
             budget: forBudget,
             luxury: forLuxurious,
         };
-        console.log(result);
+        updateNeighbourhoodsRating(result);
     };
 
     return (
         // temporary button to show all results
         <>
-            <div className='w-56 m-auto pt-3'>
-                <ButtonPrimary action={handleResult}>Резултат</ButtonPrimary>
-            </div>
-            <div className='grid gap-5 pt-4 text-center lg:grid-cols-2'>
+            <div className='grid gap-5 pt-4 text-center text-xl font-bold lg:grid-cols-2'>
                 <div>
-                    <h2>За живеене</h2>
+                    <h2 className='mb-4'>За живеене</h2>
                     <SofiaSvgMap chosen={forLiving} setChosen={setForLiving} />
                 </div>
                 <div>
-                    <h2>За инвестиция</h2>
+                    <h2 className='mb-4'>За инвестиция</h2>
                     <SofiaSvgMap chosen={forInvestment} setChosen={setForInvestment} />
                 </div>
                 <div>
-                    <h2>Бюджетен</h2>
+                    <h2 className='mb-4'>Бюджетен</h2>
                     <SofiaSvgMap chosen={forBudget} setChosen={setForBudget} />
                 </div>
                 <div>
-                    <h2>Луксозен</h2>
+                    <h2 className='mb-4'>Луксозен</h2>
                     <SofiaSvgMap chosen={forLuxurious} setChosen={setForLuxurious} />
                 </div>
+            </div>
+            <div className='m-auto w-56 pt-3'>
+                <ButtonPrimary action={handleResult}>Запиши</ButtonPrimary>
             </div>
         </>
     );
